075ae1d0d7006861555e17af22f21c3076efcd53
samid <samidsyed1720@gmail.com>
2024-09-05 15:58:44 +0530

    huzaifa

--- basic/1.js
+++ basic/1.js
@@ -1,2 +1,4 @@
 console.log('samid')
 console.log("syed samid")
+console.log("syed-samm ")
+console.log("huzaifaa")
---
+++ basic/2.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+    return a + b
+  }
+
+  let num1 = 5;
+  let num2 = 10
+  let sum = addNumbers(num1, num2)
+  console.log("The sum is: " + sum)
+  
